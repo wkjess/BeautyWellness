f57ffa8b5b9ba6b5c41879e38a663bcf05be72bd
Jessie Wong <kimjang.wong1@gmail.com>
2020-12-03 13:34:31 +0000

    updated app.js

--- app.js
+++ app.js
@@ -4,25 +4,61 @@ var http = require('http'), //This module provides the HTTP server functionaliti
     fs = require('fs'), //This module allows to work witht the file system: read and write files back
     xmlParse = require('xslt-processor').xmlParse, //This module allows us to work with XML files
     xsltProcess = require('xslt-processor').xsltProcess; //The same module allows us to utilise XSL Transformations
+    xml2js = require('xml2js'); //This module does XML to JSON conversion and also allows us to get from JSON back to XML
 
 var router = express(); //We set our routing to be handled by Express
 var server = http.createServer(router); //This is where our server gets created
 
+router.use(express.static(path.resolve(__dirname,'views'))); //We serve static content from "views" folder
+
+// Function to read in XML file and convert it to JSON
+function xmlFileToJs(filename, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+    if (err) throw (err);
+    xml2js.parseString(xmlStr, {}, cb);
+  });
+}
+
+//Function to convert JSON to XML and save it
+function jsToXmlFile(filename, obj, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  var builder = new xml2js.Builder();
+  var xml = builder.buildObject(obj);
+  fs.unlinkSync(filepath);
+  fs.writeFile(filepath, xml, cb);
+}
+
 router.get('/', function(req, res) {
 
+    res.render('index');
+
+});
+
+router.get('/get/html', function(req, res) {
+
     res.writeHead(200, {'Content-Type': 'text/html'}); //We are responding to the client that the content served back is HTML and the it exists (code 200)
+
     var xml = fs.readFileSync('BeautyWellness.xml', 'utf8'); //We are reading in the XML file
     var xsl = fs.readFileSync('BeautyWellness.xsl', 'utf8'); //We are reading in the XSL file
+
     var doc = xmlParse(xml); //Parsing our XML file
     var stylesheet = xmlParse(xsl); //Parsing our XSL file
+
     var result = xsltProcess(doc, stylesheet); //This does our XSL Transformation
+
+    xmlFileToJs('BeautyWellness.xml', function(err, result) {
+        if (err) throw (err);
+        console.log(result);
+    });
+
     res.end(result.toString()); //Send the result back to the user, but convert to type string first
 
 });
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
-
+    
     var addr = server.address();
     console.log("Server listnening at", addr.address + ":" + addr.port);
-
+    
 });
