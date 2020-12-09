f64f5c44a15d69ac50c2646a5b6cd0462e046521
Jessie Wong <kimjang.wong1@gmail.com>
2020-12-09 13:02:05 +0000

    updated files

--- app.js
+++ app.js
@@ -9,38 +9,43 @@ var http = require('http'), //This module provides the HTTP server functionaliti
 var router = express(); //We set our routing to be handled by Express
 var server = http.createServer(router); //This is where our server gets created
 
-router.use(express.static(path.resolve(__dirname,'views'))); //We serve static content from "views" folder
-
-router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be coming in as part of the URL in GET and POST requests
+router.use(express.static(path.resolve(__dirname, 'views'))); //We define the views folder as the one where all static content will be served
+router.use(express.urlencoded({ extended: true })); //We allow the data sent from the client to be coming in as part of the URL in GET and POST requests
 router.use(express.json()); //We include support for JSON that is coming from the client
 
 // Function to read in XML file and convert it to JSON
 function xmlFileToJs(filename, cb) {
-  var filepath = path.normalize(path.join(__dirname, filename));
-  fs.readFile(filepath, 'utf8', function(err, xmlStr) {
-    if (err) throw (err);
-    xml2js.parseString(xmlStr, {}, cb);
-  });
+
+    var filepath = path.normalize(path.join(__dirname, filename));
+
+    fs.readFile(filepath, 'utf8', function (err, xmlStr) {
+    
+        if (err) throw (err);
+        xml2js.parseString(xmlStr, {}, cb);
+    
+    });
 }
 
 //Function to convert JSON to XML and save it
 function jsToXmlFile(filename, obj, cb) {
-  var filepath = path.normalize(path.join(__dirname, filename));
-  var builder = new xml2js.Builder();
-  var xml = builder.buildObject(obj);
-  fs.unlinkSync(filepath);
-  fs.writeFile(filepath, xml, cb);
+    
+    var filepath = path.normalize(path.join(__dirname, filename));
+    var builder = new xml2js.Builder();
+    var xml = builder.buildObject(obj);
+    fs.unlinkSync(filepath);
+    fs.writeFile(filepath, xml, cb);
+
 }
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
 
     res.render('index');
 
 });
 
-router.get('/get/html', function(req, res) {
+router.get('/get/html', function (req, res) {
 
-    res.writeHead(200, {'Content-Type': 'text/html'}); //We are responding to the client that the content served back is HTML and the it exists (code 200)
+    res.writeHead(200, { 'Content-Type': 'text/html' }); //We are responding to the client that the content served back is HTML and the it exists (code 200)
 
     var xml = fs.readFileSync('BeautyWellness.xml', 'utf8'); //We are reading in the XML file
     var xsl = fs.readFileSync('BeautyWellness.xsl', 'utf8'); //We are reading in the XSL file
@@ -54,18 +59,31 @@ router.get('/get/html', function(req, res) {
 
 });
 
-router.post('/post/json', function(req, res) {
+router.post('/post/json', function (req, res) {
 
-    function appendJSON(obj)
-        
-        xmlFileToJs('BeautyWellness.xml', function(err, result) {
-        if (err) throw (err);
+    function appendJSON(obj) {
 
-        result.salonmenu.section[obj.sec_n].entree.push({'item':obj.item, 'price': obj.price});
+        console.log(obj)
 
-        console.log(result);
+        xmlFileToJs('BeautyWellness.xml', function (err, result) {
+            
+            if (err) throw (err);
 
+            result.salonmenu.section[obj.sec_n].entree.push({ 'item': obj.item, 'price': obj.price });
+
+            console.log(JSON.stringify(result, null, "  "));
+
+            jsToXmlFile('BeautyWellness.xml', result, function (err) {
+            
+                if (err) console.log(err);
+            
+            });
         });
+    };
+
+    appendJSON(req.body);
+
+    res.redirect('back');
 
 });
 
@@ -73,5 +91,5 @@ server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function ()
 
     var addr = server.address();
     console.log("Server listnening at", addr.address + ":" + addr.port);
-    
+
 });
